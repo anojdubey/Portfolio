1b4b8032b120dbf6df9793a74f601cffdd21ecb7
anoj <anojadubey@gmail.com>
2023-04-28 12:04:51 +0530

    second commit

---
+++ backend/schemas/workExperience.js
@@ -0,0 +1,30 @@
+export default {
+  name: 'workExperience',
+  title: 'Work Experience',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'name',
+      type: 'string',
+    },
+    {
+      name: 'time',
+      title: 'Time',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'ImageUrl',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'desc',
+      title: 'Desc',
+      type: 'string',
+    },
+  ],
+}
